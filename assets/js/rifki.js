6e56eea08b3c32cb83bcdbb9a66142bd9c08db87
rifkiard <rifkiardiansyah670@gmail.com>
2020-12-17 12:29:44 +0700

    fifth commit

--- assets/js/rifki.js
+++ assets/js/rifki.js
@@ -1,3 +1,8 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js')
+    .then(reg => console.log('sw registered', reg))
+    .catch(err => console.log('sw error', err));
+}
 
 $(function () {
   var aboutOffset = $("#about").offset().top - 70,
